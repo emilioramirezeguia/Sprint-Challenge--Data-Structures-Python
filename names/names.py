8b9f8b7ea593574f1b4a7ce1976283a250bd37e5
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-09-11 10:02:36 -0700

    First pass at names

--- names/names.py
+++ names/names.py
@@ -18,9 +18,67 @@ for name_1 in names_1:
         if name_1 == name_2:
             duplicates.append(name_1)
 
+
+class Node:
+    def __init__(self, value, next_node=None):
+        self.value = value
+        self.next_node = next
+
+    def get_value(self):
+        return self.value
+
+    def get_next_node(self):
+        return self.next_node
+
+    def set_next_node(self, next_node):
+        self.next_node = next
+
+
+class LinkedList:
+    def __init__(self, value=None):
+        self.head_node = Node(value)
+
+    def get_head_node(self):
+        return self.head_node
+
+    def add_head_node(self, new_node):
+        new_node = Node(new_node)
+        new_node.set_next_node(self.head_node)
+        self.head_node = new_node
+
+    def remove_node(self, value_to_remove):
+        current_node = self.get_head_node()
+        if current_node.get_value() == value_to_remove():
+            self.head_node = current_node.get_next_node()
+        else:
+            while current_node:
+                next_node = current_node.get_next_node()
+                if next_node.get_value() == value_to_remove:
+                    current_node.set_next_node(next_node.get_next_node())
+                    current_node = None
+                else:
+                    current_node = next_node
+
+    def contains(self, value):
+        if not self.head_node:
+            return False
+
+        current_node = self.head_node
+
+        while current_node:
+            if current_node.get_value() == value:
+                return True
+            current_node = current_node.get_next_node()
+
+        return False
+
+# Add first list to LinkedList
+for name in names_1:
+    names_node = Node(name)
+
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
