22723a6f94498ac34abade95254efab84dddff68
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-09-11 09:01:08 -0700

    First pass at Ring Buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,48 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.top_item = None
+        self.capacity = capacity
+        self.storage = [] * capacity
+        self.tail = -1
+        self.head = 0
+        self.size = 0
 
     def append(self, item):
-        pass
+        if self.is_full():
+            return None  # Buffer is full
+        else:
+            self.tail = (self.tail + 1) % self.capacity
+            self.storage[self.tail] = item
+            self.size = self.size + 1
 
     def get(self):
-        pass
+        if self.is_empty():
+            return None  # Buffer is empty
+        else:
+            tmp = self.storage[self.head]
+            self.head = (self.head + 1) % self.capacity
+
+        self.size = self.size - 1
+        return tmp
+
+    def display(self):
+        if self.is_empty():
+            print("Buffer is empty.")
+        else:
+            index = self.head
+
+            for i in range(self.size):
+                print(self.storage[index])
+                index = (index + 1) % self.capacity
+
+    # def peek(self):
+    #     if self.is_empty() is not None:
+    #         return self.top_item.get_value()
+    #     else:
+    #         print("Buffer is empty.")
+
+    def is_full(self):
+        return self.capacity == len(self.storage)
+
+    def is_empty(self):
+        return self.capacity == 0
