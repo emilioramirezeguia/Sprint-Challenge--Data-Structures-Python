a39297f52155482bf4e65ec318044b3fc643d72f
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-09-11 14:41:29 -0700

    Fix RingBuffer to pass all tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,48 +1,20 @@
 class RingBuffer:
     def __init__(self, capacity):
-        self.top_item = None
         self.capacity = capacity
-        self.storage = [] * capacity
-        self.tail = -1
-        self.head = 0
-        self.size = 0
+        self.storage = [None] * capacity
+        self.current_index = 0
 
     def append(self, item):
-        if self.is_full():
-            return None  # Buffer is full
-        else:
-            self.tail = (self.tail + 1) % self.capacity
-            self.storage[self.tail] = item
-            self.size = self.size + 1
+        # append at the head index
+        self.storage[self.current_index] = item
+        # then increment the current_index
+        self.current_index += 1
+        # don't go over capacity, so check if current_index is equal to capacity
+        if self.current_index == self.capacity:
+            self.current_index = 0
+        # if it is, reset current_index to 0
 
     def get(self):
-        if self.is_empty():
-            return None  # Buffer is empty
-        else:
-            tmp = self.storage[self.head]
-            self.head = (self.head + 1) % self.capacity
-
-        self.size = self.size - 1
-        return tmp
-
-    def display(self):
-        if self.is_empty():
-            print("Buffer is empty.")
-        else:
-            index = self.head
-
-            for i in range(self.size):
-                print(self.storage[index])
-                index = (index + 1) % self.capacity
-
-    # def peek(self):
-    #     if self.is_empty() is not None:
-    #         return self.top_item.get_value()
-    #     else:
-    #         print("Buffer is empty.")
-
-    def is_full(self):
-        return self.capacity == len(self.storage)
-
-    def is_empty(self):
-        return self.capacity == 0
+        filtered_data = [
+            element for element in self.storage if element is not None]
+        return filtered_data
